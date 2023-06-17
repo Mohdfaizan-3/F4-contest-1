617deceb68541096ff9281a3c4249d514aa0dcb5
Mohd Faizan <faizan.mohd09@gmail.com>
2023-06-17 21:50:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const { name, email, password, confirmPassword } = formData;
+
+    // if (!name || !email || !password || !confirmPassword) {
+    //   setError(true);
+    //   setSuccess(false);
+    // } else {
+    //   setError(false);
+    //   setSuccess(true);
+    // }
+
+    if (!name || !email || !password || !confirmPassword) {
+      setError(true);
+      setSuccess(false);
+    } else if (password !== confirmPassword) {
+      setError(true);
+      setMessage("password and confirm password do not match");
+      setSuccess(false);
+    } else {
+      setError(false);
+      setSuccess(true);
+      setMessage("");
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <h1>Signup</h1>
+        <input
+          type="text"
+          placeholder="Full Name"
+          id="name"
+          name="name"
+          // value={formData.name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          name="email"
+          // value={formData.email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          name="password"
+          // value={formData.password}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="Confirm Password"
+          id="confirmPassword"
+          name="confirmPassword"
+          // value={formData.confirmPassword}
+          onChange={handleInputChange}
+        />
+
+        {message && <div className="message">{message}</div>}
+        {error ? (
+          <div className="display-error">Error: All fields are mandatory</div>
+        ) : null}
+        {success ? (
+          <div className="display-success">Successfully signed up</div>
+        ) : null}
+        <button type="submit">Signup</button>
+      </form>
     </div>
   );
 }
